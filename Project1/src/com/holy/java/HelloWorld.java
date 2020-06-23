669d55ab4b9b6d103458fca39b7d47d1a31a6d30
holy <592064112@qq.com>
2020-06-23 09:44:33 +0800

    修改helloworld中的输出语句

--- Project1/src/com/holy/java/HelloWorld.java
+++ Project1/src/com/holy/java/HelloWorld.java
@@ -1,11 +1,9 @@
 package com.holy.java;
 
-import java.util.Arrays;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
 
-
+        System.out.println("hellogit");
     }
 }
